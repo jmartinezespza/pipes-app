0a2f674404d68c23d69a3406218b3d9717457d20
Jhonny Martínez <jmartinezespza@gmail.com>
2021-07-05 15:21:25 -0500

    implementando menubar de primeng

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 
 // PrimeNg
-import {ButtonModule} from 'primeng/button';
-import {CardModule} from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { MenubarModule } from 'primeng/menubar';
 
 @NgModule({
-  exports: [
-    ButtonModule,
-    CardModule
-  ]
+  exports: [ButtonModule, CardModule, MenubarModule],
 })
-export class PrimeNgModule { }
+export class PrimeNgModule {}
