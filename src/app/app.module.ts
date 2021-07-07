3b3e04841820e69a4c8f814197987c896ed080fd
Jhonny <jmartinezespza@gmail.com>
2021-07-06 23:26:35 -0500

    Idioma espanol peru a la aplicacion. locale global.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRouterModule } from './app-router.module';
 
@@ -6,10 +6,17 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { VentasModule } from './ventas/ventas.module';
 
+// cambiar el locale de la app
+import localeEs from '@angular/common/locales/es-PE'
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEs);
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, SharedModule, AppRouterModule, VentasModule],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'es-PE'}
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
