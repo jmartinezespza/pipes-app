289b5ff74d0efda62f627bf7a90d2c31e927a12d
Jhonny Martínez Espinoza <jmartinezespza@gmail.com>
2021-07-18 00:32:57 -0500

    parametrizando pipe personalizado

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -6,17 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent {
 
   enMayuscula: boolean = true;
+  ordenarPor: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  heroes: Heroe[] = [
+    {
+      nombre: 'SuperMan',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Capitan América',
+      vuela: false,
+      color: Color.azul
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde
+    },
+  ];
 
   cambiar() {
     this.enMayuscula = !this.enMayuscula;
+  }
+
+  cambiarOrden(valor: string) {
+    this.ordenarPor = valor;
   }
 
 }
